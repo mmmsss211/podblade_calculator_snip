576997793a66e19e772973321659ec360900cf41
Percival Ian Muico <mmmsss211@gmail.com>
2020-11-16 17:04:25 +0800

    Unified scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -95,3 +95,16 @@ $("#pod_calc_close_calculator").on("click", function() {
     $(".pod_calc_popup_calculator").removeClass("active");
     $(".pod_calc_popup_container").removeClass("active");
 });
+
+const
+range = document.getElementById('pod_calc_markup'),
+rangeV = document.getElementById('pod_calc_rangeV'),
+setValue = ()=>{
+const
+newValue = Number( (range.value - range.min) * 100 / (range.max - range.min) ),
+newPosition = 10 - (newValue * 0.2);
+rangeV.innerHTML = `<span>${range.value}x</span>`;
+rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+};
+document.addEventListener("DOMContentLoaded", setValue);
+range.addEventListener('input', setValue);
